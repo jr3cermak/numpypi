a6df7f64622c23216999b92cfa9ce81ace50bde9
Alistair Adcroft <adcroft+github@gmail.com>
2019-06-04 08:50:53 -0400

    More fns

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 import pypi
 
-pypi.reciprocal(.2, verbose=True)
-pypi.sqrt(5.0, verbose=True)
+re = pypi.reciprocal(.2, verbose=True)
+sq = pypi.sqrt(5.0, verbose=True)
+pi = pypi.pi(verbose=True)
+si = pypi.sine(0.25*pi, verbose=True)
+co = pypi.cosine(0.25*pi, verbose=True)
