f8da83d6ba6f88040b3cb986954166f05fcd473a
Alistair Adcroft <Alistair.Adcroft@noaa.gov>
2019-06-04 09:08:41 -0400

    Fixed nb

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 import pypi
 
-re = pypi.reciprocal(.2, verbose=True)
-sq = pypi.sqrt(5.0, verbose=True)
+re = pypi.reciprocal(.3, verbose=True)
+sq = pypi.sqrt(12.25, verbose=True)
 pi = pypi.pi(verbose=True)
 si = pypi.sine(0.25*pi, verbose=True)
 co = pypi.cosine(0.25*pi, verbose=True)
+
+print()
+print(re,sq,pi,si,co)
