c208131c4725f71685bcdb7c286fa3b732a095fa
Alistair Adcroft <Alistair.Adcroft@noaa.gov>
2019-06-07 09:22:34 -0400

    Make pnumpy compatible with python2

--- pnumpy.py
+++ pnumpy.py
@@ -10,14 +10,14 @@ from numpy import *
 import ignore_this as _numpy
 
 global __default_bits
-__default_bits = 0
+__default_bits = None
 
 def round_lastbits(x, bits=None):
     if bits is None:
         bits = __default_bits
-        if bits==0: return x # Only return unrounded argument if bits==None and __default_bits==0
     else:
         assert bits>=0, 'Optional argument "bits" must be non-negative'
+    if bits is None: return x # Return un-rounded argument if bits==None and __default_bits==None
     bits = int(bits)
     if type(x) is ndarray:
         m,e = frexp( x ) # Mantissa and twos exponent
@@ -26,7 +26,7 @@ def round_lastbits(x, bits=None):
             m = array( m * ( 2**(bits_to_shift) ) ,dtype=float).round() * ( 2**(-bits_to_shift))
             return ldexp( m, e )
         raise Exception('Operation not permitted on ndarray dtype "'+x.dtype+'"')
-    elif type(x) == float:
+    elif type(x) == float or type(x) == _numpy.float64:
         return float( round_lastbits( array([x], dtype=float), bits=bits ) )
     raise Exception('Operation not permitted on type "'+str(type(x))+'"')
 
@@ -34,26 +34,30 @@ def set_rounding_bits(bits):
     global __default_bits
     __default_bits = int(bits)
 
-def sin(*args, bits=None, **kwargs):
-    if bits is None: bits = __default_bits
-    return round_lastbits( _numpy.sin( *args, **kwargs ), bits=bits )
+def unset_rounding_bits():
+    global __default_bits
+    __default_bits = None
 
-def cos(*args, bits=None, **kwargs):
-    if bits is None: bits = __default_bits
-    return round_lastbits( _numpy.cos( *args, **kwargs ), bits=bits )
+def sin(*args, **kwargs):
+    global __default_bits
+    return round_lastbits( _numpy.sin( *args, **kwargs ), bits=__default_bits )
 
-def tan(*args, bits=None, **kwargs):
-    if bits is None: bits = __default_bits
-    return round_lastbits( _numpy.tan( *args, **kwargs ), bits=bits )
+def cos(*args, **kwargs):
+    global __default_bits
+    return round_lastbits( _numpy.cos( *args, **kwargs ), bits=__default_bits )
 
-def arcsin(*args, bits=None, **kwargs):
-    if bits is None: bits = __default_bits
-    return round_lastbits( _numpy.arcsin( *args, **kwargs ), bits=bits )
+def tan(*args, **kwargs):
+    global __default_bits
+    return round_lastbits( _numpy.tan( *args, **kwargs ), bits=__default_bits )
 
-def arccos(*args, bits=None, **kwargs):
-    if bits is None: bits = __default_bits
-    return round_lastbits( _numpy.arccos( *args, **kwargs ), bits=bits )
+def arcsin(*args, **kwargs):
+    global __default_bits
+    return round_lastbits( _numpy.arcsin( *args, **kwargs ), bits=__default_bits )
 
-def arctan(*args, bits=None, **kwargs):
-    if bits is None: bits = __default_bits
-    return round_lastbits( _numpy.arctan( *args, **kwargs ), bits=bits )
+def arccos(*args, **kwargs):
+    global __default_bits
+    return round_lastbits( _numpy.arccos( *args, **kwargs ), bits=__default_bits )
+
+def arctan(*args, **kwargs):
+    global __default_bits
+    return round_lastbits( _numpy.arctan( *args, **kwargs ), bits=__default_bits )
